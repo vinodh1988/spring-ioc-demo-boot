4af477c7f9720e3bde6f3e84ed83cd99382e0fa1
vinodh1988 <k.c.vinodh@hotmail.com>
2024-11-11 17:02:03 +0530

    integrated

--- src/main/java/com/iocapp/integraton/Integrated.java
+++ src/main/java/com/iocapp/integraton/Integrated.java
@@ -16,6 +16,9 @@ public class Integrated {
 		System.out.println("Integrated got Instantiated");
 		System.out.println(store); // at this point of time dependency injection not happened
 	}
+	public Integrated(){
+		System.out.println(store);
+	}
 	//post construct runs automatically after constructor and all properties are intialized
 	@PostConstruct
 	public void init() {
