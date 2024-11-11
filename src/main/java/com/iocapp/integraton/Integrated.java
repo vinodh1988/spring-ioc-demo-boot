f868e7e136f6aa3a129f667aacd477a99b73434a
vinodh1988 <k.c.vinodh@hotmail.com>
2024-11-11 16:58:16 +0530

    integrated

--- src/main/java/com/iocapp/integraton/Integrated.java
+++ src/main/java/com/iocapp/integraton/Integrated.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 import com.iocapp.components.DataStore;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class Integrated {
   @Autowired
@@ -12,5 +14,14 @@ public class Integrated {
 	
 	{
 		System.out.println("Integrated got Instantiated");
+		System.out.println(store); // at this point of time dependency injection not happened
+	}
+	//post construct runs automatically after constructor and all properties are intialized
+	@PostConstruct
+	public void init() {
+		System.out.println("In post construct of Integrated");
+		System.out.println(store); //autowired
+		store.store();
+		
 	}
 }
